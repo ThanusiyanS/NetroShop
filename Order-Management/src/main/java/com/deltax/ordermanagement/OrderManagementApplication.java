f506d644a444589708b5736ff2038a21f0445ad7
ThanusiyanS <thanusiyan295@gmail.com>
2023-11-04 21:17:07 +0530

    Eureka client final update

--- Order-Management/src/main/java/com/deltax/ordermanagement/OrderManagementApplication.java
+++ Order-Management/src/main/java/com/deltax/ordermanagement/OrderManagementApplication.java
@@ -2,12 +2,12 @@ package com.deltax.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderManagementApplication.class, args);
     }
-
 }
