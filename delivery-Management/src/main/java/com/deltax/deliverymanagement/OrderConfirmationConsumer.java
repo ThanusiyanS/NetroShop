9110bd40d0a9732e794e8613bd7d306a4215973b
mathesh <matheshyogeswaran@gmail.com>
2023-11-09 14:59:48 +0530

    rabbit

--- delivery-Management/src/main/java/com/deltax/deliverymanagement/OrderConfirmationConsumer.java
+++ delivery-Management/src/main/java/com/deltax/deliverymanagement/OrderConfirmationConsumer.java
@@ -3,7 +3,10 @@ package com.deltax.deliverymanagement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class OrderConfirmationConsumer {
--- email-service/src/main/java/com/deltax/emailservice/EmailService.java
+++ email-service/src/main/java/com/deltax/emailservice/EmailService.java
@@ -1,9 +1,9 @@
 package com.deltax.emailservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
